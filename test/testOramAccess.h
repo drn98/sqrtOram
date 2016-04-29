417dacaf9f884018b66c0436724e0aa51396b8da
Samee Zahur <samee@virginia.edu>
2016-04-29 13:46:52 -0400

    testOramAccess: Added autogeneration of data

--- test/testOramAccess.h
+++ test/testOramAccess.h
@@ -1,8 +1,10 @@
 #pragma once
+#include<stdbool.h>
 typedef struct
 { int size;
   int *content,*indices,*outputs;
   enum { oramTypeLin,oramTypeSqrt,oramTypeCkt } type;
+  bool detailedOut;
 } TestOramAccessIO;
 
 void testOramAccess(void* vargs);
