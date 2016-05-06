a4f3b5a0d0347af6edf9408a1a4b0a3ea2fc7506
Samee Zahur <samee@virginia.edu>
2016-05-06 15:21:56 -0400

    Refactoring command line parsing

--- util/util.c
+++ util/util.c
@@ -25,3 +25,41 @@ void connectOrDie(ProtocolDesc* pd,const char* server,const char* port)
     setCurrentParty(pd,2);
   }
 }
+
+int
+cmdFindFirst(const char* search,const char* alts[],int n)
+{
+  int i;
+  for(i=0;i<n;++i) if(strcmp(search,alts[i])==0) return i;
+  return -1;
+}
+
+bool
+isPrefix(const char* prefix,const char* str)
+{ int i;
+  for(i=0;;++i)
+  { if(str[i]==0) return prefix[i]==0;
+    if(prefix[i]==0) return true;
+    if(str[i]!=prefix[i]) return false;
+  }
+}
+bool
+checkPrefix(const char* alts[],int n)
+{ int i,j;
+  for(i=0;i<n;++i) for(j=0;j<n;++j) if(i!=j) if(isPrefix(alts[i],alts[j]))
+    return false;
+  return true;
+}
+
+int
+cmdFindFirstPrefix(const char* search,const char* alts[],int n)
+{
+  assert(!checkPrefix(alts,n));
+  int i,res=-1,n;
+  for(i=0;i<n;++i) if(isPrefix(search,alts[i]))
+  { if(res==-1) res=i;
+    else return -1;
+  }
+  return i;
+}
+
--- util/util.h
+++ util/util.h
@@ -3,3 +3,19 @@
 
 void connectOrDie(ProtocolDesc* pd,const char* server,const char* port);
 double wallClock();
+
+int cmdFindFirst(const char* search,const char* alts[],int n);
+int cmdFindFirstPrefix(const char* search,const char* alts[],int n);
+
+typedef enum
+{ oramTypeNone=-1,
+  oramTypeLin,
+  oramTypeSqrt,
+  oramTypeCkt
+} OramType;
+
+static inline OramType
+oramTypeFromString(const char* s)
+{ const char* types[] = {"linear","sqrt","ckt"};
+  return cmdFindFirstPrefix(s,types,sizeof(types)/sizeof(*types));
+}
